680fa6df7ad1e352cbeafe8b56c139fda4f75786
Egor Batalov <AxariumSmart@yandex.ru>
2025-05-31 18:49:43 +0700

    Тестовый коммит

--- src/main/java/ru/yandex/practicum/filmorate/util/IdGenerator.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.util;
-
-public class IdGenerator {
-
-    public static long generateId(long currentId) {
-        return ++currentId;
-    }
-} // Комментарий
