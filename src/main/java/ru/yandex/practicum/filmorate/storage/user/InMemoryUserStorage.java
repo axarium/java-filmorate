32050cfb25690579de61304cf03c977b329a991f
Egor Batalov <AxariumSmart@yandex.ru>
2025-05-31 18:59:16 +0700

    Тестовый коммит

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -23,7 +23,7 @@ public class InMemoryUserStorage implements UserStorage {
     @Override
     public User update(User user) {
         users.put(user.getId(), user);
-        return user;
+        return user; // Комментарий
     }
 
     @Override
